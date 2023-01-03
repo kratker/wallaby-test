58f1a27ea19d48754ae1958192d4a2acfb611827
kratker <40059950+kratker@users.noreply.github.com>
2023-01-03 16:42:40 +0100

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -2,17 +2,6 @@
 module.exports = function (w) {
     return {
         files:["models/plugins/business-sharing-strategy/isBusinessSharingStrategy.ts"],
-        // tests: [{pattern: "models/plugins/business-sharing-strategy/isBusinessSharingStrategy.test.ts",instrument:true, load:true}],
-        //files: [
-        //'!/**/*.test.ts',
-        //'!/**/*.js',
-        //'/**/*.ts',
-        //{pattern:"node_modules/**/*",ignore:true},
-        // {pattern:"public/**/*",ignore:true}
-
-        //],
-
-        // tests: [ {pattern:'**/*.test.ts', instrument:true, load:true},'!/**/*.test.js'],
         trace:true,
         debug:true,
         autoDetect:true,
